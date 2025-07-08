dbdff924b5e51e61804c6d75524e20df1ec5f2c7
samata-saha <samatasaha03@gmail.com>
2025-07-08 14:55:15 +0530

    delete folder

--- assignments/11th_july/app.js
+++
@@ -1,9 +0,0 @@
-const practice = require('./9th_july/questions.js');
-
-practice.printEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log("Factorial of 4:", practice.factorial(4));
-practice.multiplicationtable(5);
-console.log("Sum of first 10 natural numbers:", practice.sumOfNaturalNumbers(10));
-console.log("Largest number:", practice.findLargestNumber([1, 2, 3, -4, -5, -6]));
-console.log("2024 is leap year?", practice.isleapyear(2024));
-console.log("2025 is leap year?", practice.isleapyear(2025));
